0149a835a12ad58f3e56dacc5680e13b919725a5
gradampl <gr_adam@o2.pl>
2018-01-17 19:13:28 +0100

    Add files via upload

---
+++ ConsoleApplication19/ConsoleApplication19/ISprawdzanie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication19
+{
+    interface ISprawdzanie
+    {
+        bool Sprawdz(string p1);
+    }
+}
---
+++ ConsoleApplication19/ConsoleApplication19/Kraj.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication19
+{
+    class Kraj:IComparable<Kraj>
+    {
+        static Kraj kraj;
+        string nazwa;
+
+        public string Nazwa
+        {
+            get { return nazwa; }
+            set { nazwa = value; }
+        }
+        int powierzchnia;
+
+        private Kraj(string nazwa, int powierzchnia)
+        {
+            this.nazwa = nazwa;
+            this.powierzchnia = powierzchnia;
+        }
+
+        public static Kraj UtworzKraj(string nazwa,int powierzchnia)
+        {
+            kraj=new Kraj(nazwa,powierzchnia);
+            return kraj;
+        }
+
+        public override string ToString()
+        {
+            return "Kraj: "+nazwa+","+"powierzchnia: "+powierzchnia;
+        }
+
+        public int CompareTo(Kraj other)
+        {
+            if (this.powierzchnia == other.powierzchnia)
+                return other.nazwa.CompareTo(this.nazwa);
+            else
+            return this.powierzchnia.CompareTo(other.powierzchnia);
+            
+
+        }
+
+         
+    }
+}
